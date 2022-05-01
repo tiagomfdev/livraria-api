f8fb247a74aca4040103c496ecf6e7fea3b3b045
Tiago Machado Flor <xtiago.br0@gmail.com>
2022-05-01 14:46:30 -0300

    alterado string de conexao do mongo

--- repositories/mongo.db.js
+++ repositories/mongo.db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connect() {
   const uri =
-    "mongodb+srv://maluskao:igti@cluster0.xdis7.mongodb.net/petshop?retryWrites=true&w=majority";
+    "mongodb+srv://maluskao:igti@cluster0.xdis7.mongodb.net/bookstore?retryWrites=true&w=majority";
   return await mongoose.connect(uri);
 }
 
